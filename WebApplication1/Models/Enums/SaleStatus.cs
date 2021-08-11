b522a0e87dff2a2401c7699c4b9f484c5d23d0d2
Everton Ferreira <everton.souzaferreira@outlook.com>
2021-08-11 06:34:25 +0200

    Other entities and second migration

---
+++ WebApplication1/Models/Enums/SaleStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public enum SaleStatus : int
+    {
+        Pendinh = 0,
+        Billied = 1,
+        Canceled = 2
+
+    }
+}
